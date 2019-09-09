ef4af07236ff71d1decd4417d6f10bd6b5523a69
k38 <matumoto.stc@gmail.com>
2019-09-09 23:03:49 +0900

    Update: delay

--- animation_fadein/index.js
+++ animation_fadein/index.js
@@ -7,18 +7,19 @@ function start() {
     const items = document.querySelector(".items");
     for ( let j=0 ; j<number ; j++ ) {
         const div = document.createElement("div");
+        const b = j * 0.1;
+        const a = 1.0 - b;
         for ( let i=0 ; i<50 ; i++ ) {
-            const span = createSpan(i, j);
+            const span = createSpan(i, j, a, b);
             div.appendChild(span);
         }
         items.appendChild(div);
     }
 }
 
-function createSpan(i, j) {
-    const b = j * 0.1;
-    const a = 1.0 - b;
-    const delay = a * ( i / 50 ) + b;
+function createSpan(i, j, a, b) {
+    const delay = Math.round( ( a * i * i / 2500 + b ) * 10 ) / 10;
+    console.log(delay);
     let span = document.createElement("span");
     span = setStyles(span, {
         display: "inline-block",
