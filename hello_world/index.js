eebafb4103740a285c652a507cf10867fbeb3567
k38 <matumoto.stc@gmail.com>
2019-11-10 13:23:28 +0900

    Add: simpleParallax

--- hello_world/index.js
+++ hello_world/index.js
@@ -1,4 +1,4 @@
-window.addEventListener("load", ()=>{
+window.addEventListener("DOMContentLoaded", ()=>{
     const span = document.querySelector("span");
     span.textContent = "HelloWorld";
 });
---
+++ simpleParallax/index.js
@@ -0,0 +1,16 @@
+// https://simpleparallax.com/
+
+// let instance;
+
+// window.addEventListener("DOMContentLoaded", ()=>{
+//     const images = document.querySelectorAll("img");
+//     const options = {
+//         transition: "cubic-bezier(0,0,0,1)"
+//     };
+//     // instance = new simpleParallax(images);
+//     instance = new simpleParallax(images, options);
+// });
+
+// function end() {
+//     instance.destroy();
+// }
