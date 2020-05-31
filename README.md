14896e845fe623926e7ec7f6ee7ac4366089840c
k38 <matumoto.stc@gmail.com>
2020-05-31 13:48:56 +0900

    Add: audioContext

--- README.md
+++ README.md
@@ -2,3 +2,4 @@
 
 - [hello_world](https://k38.github.io/vanillaJs/hello_world)
 - [animation_fadein](https://k38.github.io/vanillaJs/animation_fadein)
+- [audioContext](https://k38.github.io/vanillaJs/audioContext)
---
+++ audioContext/index.js
@@ -0,0 +1,23 @@
+//@ts-check
+// document.addEventListener('DOMContentLoaded', DOMContentLoaded);
+
+// function DOMContentLoaded(){
+//     var context = new AudioContext();
+//     document.querySelector('button').addEventListener('click', function () {
+//         context.resume().then(() => {
+//             console.log('Playback resumed successfully');
+//         });
+//     });
+// }
+
+// var context;
+
+// window.onload = function () {
+//     context = new AudioContext();
+// }
+
+// document.querySelector('button').addEventListener('click', function () {
+//     context.resume().then(() => {
+//         console.log('Playback resumed successfully');
+//     });
+// });
